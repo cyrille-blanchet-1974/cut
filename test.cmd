d83da48600cd68e6fffaa2cb6b140a14350fa892
cyrille-blanchet-1974 <cyrille.blanchet@gmail.com>
2022-01-22 10:58:56 +0100

    add new tests  TODO: correct panic and manage errors correctly

--- test.cmd
+++ test.cmd
@@ -1,20 +1,23 @@
 @echo off
 call build.cmd
+
+set prg=.\targe\debug\cut.exe
+
 echo 12345678901234567890123456>test.fic
 echo abcdefghijklmnopqrstuvwxyz>>test.fic
 echo ABCDEFGHIJKLMNOPQRSTUVWZXYZ>>test.fic
-.\target\debug\cut.exe -c 10-15 test.fic
+%prg% -c 10-15 test.fic
 echo expected: 
 echo 12345
 echo klmno
 echo KLMNO
 pause
 
-.\target\debug\cut.exe -c 40-45 test.fic
+%prg% -c 40-45 test.fic
 echo expected: no data
 pause
 
-.\target\debug\cut.exe -c 5-4 test.fic
+%prg% -c 5-4 test.fic
 echo expected: error
 pause
 
@@ -22,6 +25,28 @@ echo 1:2:3:4:5:6>test.fic
 echo a:b:c:d:e:f>>test.fic
 echo A:B:C:D:E:F>>test.fic
 
-.\target\debug\cut.exe -f 2 -d : test.fic
+%prg% -f 2 -d : test.fic
 echo expected: 2 b B
+pause
+
+
+%prg% -f 8 -d : test.fic
+echo expected: nothing
+pause
+
+
+%prg% -f 0 -d : test.fic
+echo expected: error
+pause
+
+%prg% -f -10 -d : test.fic
+echo expected: error
+pause
+
+%prg% -f 2 -d ;  test.fic
+echo expected: nothing
+pause
+
+%prg% -f 1 -d ; test.fic
+echo expected: full file
 pause
--- test.sh
+++ test.sh
@@ -5,12 +5,53 @@ function pause {
 
 
 ./build.sh
+
+export prg=./target/debug/cut
+
 echo 12345678901234567890123456>test.fic
 echo abcdefghijklmnopqrstuvwxyz>>test.fic
 echo ABCDEFGHIJKLMNOPQRSTUVWZXYZ>>test.fic
-./target/debug/cut -c 10-15 test.fic
+$prg -c 10-15 test.fic
 echo expected: 
 echo 12345
 echo klmno
 echo KLMNO
 pause
+
+$prg -c 40-45 test.fic
+echo expected: no data
+pause
+
+$prg -c 5-4 test.fic
+echo expected: error
+pause
+
+echo 1:2:3:4:5:6>test.fic
+echo a:b:c:d:e:f>>test.fic
+echo A:B:C:D:E:F>>test.fic
+
+$prg -f 2 -d : test.fic
+echo expected: 2 b B
+pause
+
+$prg -f 8 -d : test.fic
+echo expected: nothing
+pause
+
+
+$prg -f 0 -d : test.fic
+echo expected: error
+pause
+
+$prg -f -10 -d : test.fic
+echo expected: error
+pause
+
+$prg -f 2 -d ;  test.fic
+echo expected: nothing
+pause
+
+$prg -f 1 -d ; test.fic
+echo expected: full file
+pause
+
